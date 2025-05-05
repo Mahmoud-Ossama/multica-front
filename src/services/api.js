3beaa0473fa2fe7f30a037e36b1a20d481442a4f
Mahmoud <vip.m.osama@gmial.com>
2025-05-05 20:23:53 +0300

    Initial commit

---
+++ src/services/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { API_BASE_URL } from '../config';
+
+// Create an axios instance with the base URL
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Add a request interceptor to handle authentication
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
